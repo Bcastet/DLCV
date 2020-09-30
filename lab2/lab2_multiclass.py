a78fd4949bfd61c0a491b0bc6499a3a5f2ceca57
bcastet <benjamin.castet@etu.u-bordeaux.fr>
2020-09-30 16:53:17 +0200

    lab 2 update

--- lab2/lab2_multiclass.py
+++ lab2/lab2_multiclass.py
@@ -1,14 +1,12 @@
 from __future__ import print_function
 import numpy as np
-import math
+
 #In this first part, we just prepare our data (mnist) 
 #for training and testing
 
 #import keras
 from tensorflow.keras.datasets import mnist
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
-
-
 num_pixels = X_train.shape[1] * X_train.shape[2]
 X_train = X_train.reshape(X_train.shape[0], num_pixels).T
 X_test = X_test.reshape(X_test.shape[0], num_pixels).T
@@ -22,28 +20,23 @@ X_train  = X_train / 255
 X_test  = X_test / 255
 
 
-#We want to have a binary classification: digit 0 is classified 1 and 
-#all the other digits are classified 0
 
+# one-hot encode labels
+digits = 10
 
+def one_hot_encode(y, digits):
+    examples = y.shape[0]
+    y = y.reshape(1, examples)
+    Y_new = np.eye(digits)[y.astype('int32')]  #shape (1, 70000, 10)
+    Y_new = Y_new.T.reshape(digits, examples)
+    return Y_new
 
-y_new = np.zeros(y_train.shape)
-y_new[np.where(y_train==3.)[0]] = 1
-y_train = y_new
-
-y_new = np.zeros(y_test.shape)
-y_new[np.where(y_test==3.)[0]] = 1
-y_test = y_new
-
-
-y_train = y_train.T
-y_test = y_test.T
-
-
-m = X_train.shape[1] #number of examples
+y_train=one_hot_encode(y_train, digits)
+y_test=one_hot_encode(y_test, digits)
 
 #Now, we shuffle the training set
 np.random.seed(138)
+m = X_train.shape[1]
 shuffle_index = np.random.permutation(m)
 X_train, y_train = X_train[:,shuffle_index], y_train[:,shuffle_index]
 
@@ -51,24 +44,23 @@ def sigmoid(x):
   return 1 / (1 + np.exp(-x))
 
 def compute_loss(y,y_hat):
-    l = - 1./m*(np.sum(np.multiply(y,np.log(y_hat))) + np.sum(np.multiply(1-y,np.log(1-y_hat)))) 
-    return l
+    return -np.sum(np.multiply(y,np.log(Yhat)))
 
 X = X_train
 Y = y_train
 
 n = X_train.shape[0]
 
-lr = 0.01
+lr = 1
 nh = 64
 
-W1 = np.random.randn(nh,n) * 0.01
+W1 = np.random.randn(nh,n) 
 b1 = np.zeros((nh,1))
 
-W2 = np.random.randn(1,nh) * 0.01
-b2 = np.zeros((1,1))
+W2 = np.random.randn(10,nh) 
+b2 = np.zeros((10,1))
 
-epochs = 100
+epochs = 150
 
 for i in range(epochs):
     Z1 = np.matmul(W1,X) + b1
@@ -102,11 +94,16 @@ Y1 = sigmoid(Z1)
 Z2 = np.matmul(W2,Y1) + b2
 Yhat = sigmoid(Z2)
 loss = compute_loss(y_test,Yhat)
-accurate = 0
-print(y_test.shape)
-for i in range(y_test.shape[1]):
-  if (y_test[0][i] == 0 and Yhat[0][i]<0.5) or (y_test[0][i] == 1 and Yhat[0][i]>0.5):
-    accurate += 1
-print("Final accuracy:",accurate/y_test.shape[1])
+accurate =0
+
+y_test = y_test.T
+Yhat = Yhat.T
+
+for i in range(len(y_test)):
+    if np.argmax(y_test[i]) == np.argmax(Yhat[i]):
+        accurate+=1
+
+print("Final accuracy:",accurate/len(y_test))
+
 
 
